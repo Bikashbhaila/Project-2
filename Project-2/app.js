94d505eaf7abc188e8942108808f05003497138d
Nyonjee Zeogar <nyonjeezeogar@gmail.com>
2020-03-31 22:34:59 -0500

    Update app.js

--- Project-2/app.js
+++ Project-2/app.js
@@ -1,8 +1,10 @@
+require("dotenv").config();
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var exphbs = require("express-handlebars");
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
