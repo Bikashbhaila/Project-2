5d758b6dc0ec7c0ea7806b8b92a18271361c5d8d
PROX-E <lichlord18@gmail.com>
2020-03-31 19:23:31 -0500

    Create Loading Page

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
+function onReady(callback) {
+    var intervalID = window.setInterval(checkReady, 3000);
+
+    function checkReady() {
+        if (document.getElementsByTagName('body')[0] !== undefined) {
+            window.clearInterval(intervalID);
+            callback.call(this);
+        }
+    }
+}
+
+function show(id, value) {
+    document.getElementById(id).style.display = value ? 'block' : 'none';
+}
+
+onReady(function () {
+    show('map', true);
+    show('loading', false);
+});
+
 
 // Corner Navigation Buttons
 document.addEventListener('DOMContentLoaded', function() {
