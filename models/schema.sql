5f5a538ac2aec5519282095efa9542399df9c73f
Bikashbhaila <bikash.bhaila@outlook.com>
2020-04-02 20:58:54 -0500

    add seeds to schema file

--- models/schema.sql
+++ models/schema.sql
@@ -21,4 +21,13 @@ CREATE TABLE City (
 );
 
 
+INSERT INTO Main_map (Map_Name) VALUES ("Bravoos");
+INSERT INTO Main_map (Map_Name) VALUES ("Westboro");
+INSERT INTO Main_map (Map_Name) VALUES ("WitchTown");
+INSERT INTO Main_map (Map_Name) VALUES ("Hamburg");
+
+INSERT INTO City (City_Name, Is_Capital, Has_Castle, Has_Market) VALUES ("Mancity", "True", "True", "False");
+INSERT INTO City (City_Name, Is_Capital, Has_Castle, Has_Market) VALUES ("OldYork", "True", "True", "False");
+INSERT INTO City (City_Name, Is_Capital, Has_Castle, Has_Market) VALUES ("NewYork", "True", "True", "False")
+INSERT INTO City (City_Name, Is_Capital, Has_Castle, Has_Market) VALUES ("Richland", "True", "True", "False")
 
