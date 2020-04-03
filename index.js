2fd5314d4e4882831576e1caff597843bd794de2
Alexrobbie <robinsonmail25@gmail.com>
2020-04-02 20:01:01 -0500

    Added Function Templates

--- index.js
+++ index.js
@@ -11,18 +11,20 @@ document.addEventListener('DOMContentLoaded', function() {
   let cityData=[];
   let city=0;
 
+//Initialize variables
+let map=[]; //The X column
+let mapRow=[];  //The Y column
+let x=0; //Random X Position
+let y=0; //Random Y Position
+let MapID=0; //Map ID used to give each pixle a positioning
+let xRan=0;
+let yRan=0;
 
+//Creates a new Map Array
 function start(){
     
 
-//Initialize variables
-    let map=[]; //The X column
-    let mapRow=[];  //The Y column
-    let x=0; //Random X Position
-    let y=0; //Random Y Position
-    let MapID=0; //Map ID used to give each pixle a positioning
-    let xRan=0;
-    let yRan=0;
+
     
 //Generates the Array
     for (i=0;i<400;i++) {
@@ -59,7 +61,7 @@ for(o=0;o<=10;o++){
 }
     
 //City Placer
-for(o=0;o<=10;o++){
+for(o=0;o<=9;o++){
     x=Math.floor(Math.random() * 399)+1;
     y=Math.floor(Math.random() * 199)+1;
 
@@ -67,7 +69,7 @@ for(o=0;o<=10;o++){
 
         map[x][y]=2;
 
-        let city={
+         city={
             population: Math.floor(Math.random() * 100000)+1,
             name: "Enter City Here", 
             capital: "false",
@@ -110,7 +112,14 @@ for(o=0;o<=10;o++){
 //Consols the Array for Debuging
     //console.table(map);
 
-cityNum=0;
+placeMap();
+cityClick();
+grassClick();
+}
+//Turns the Array into something the user can see
+function placeMap(){
+    console.table(map);
+    cityNum=0;
 //Converts the array into a readable 
 for (l=0;l<map.length;l++){
     for (u=0;u<200;u++){
@@ -181,11 +190,8 @@ for (l=0;l<map.length;l++){
         MapID++;
     }
 }
-cityClick();
-grassClick();
 }
-
-
+//Shows Data when the user clicks on a city
 function cityClick() {
     $(".city").click(function(){
          city=$(this).attr('class')[9]
@@ -207,6 +213,7 @@ function cityClick() {
     });
 });
 }
+//Stops showing city data when clicking on the grass
 function grassClick() {
     $(".grass").click(function(){
         $(".click").empty();
@@ -214,5 +221,37 @@ function grassClick() {
 
 }
 
+//Save Map
+
+function save(){
+
+//Send the Following to MYSQL
+
+    //Array Data
+    for (i=0;i<map.length;i++){
+        
+    }
+
+    //City Data
+    for (i=0;i<cityData;i++){
+        
+    }
+}
+
+//Loads Map
+function load(){
+
+//Get the Following from MYSQL
+
+    //Array Data
+    for (i=0;i<400;i++){
+        
+    }
+
+    //City Data
+    for (i=0;i<10;i++){
+        
+    }
+}
     
 start();
