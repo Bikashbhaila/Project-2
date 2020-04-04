8918b4dce74781a889f6b2a78c3f2bc69f93c5ea
PROX-E <lichlord18@gmail.com>
2020-04-03 19:19:34 -0500

    seeds to seperate file

---
+++ db/seeds.sql
@@ -0,0 +1,10 @@
+USE 'maps_db';
+INSERT INTO Main_map (Map_Name) VALUES ("Bravoos");
+INSERT INTO Main_map (Map_Name) VALUES ("Westboro");
+INSERT INTO Main_map (Map_Name) VALUES ("WitchTown");
+INSERT INTO Main_map (Map_Name) VALUES ("Hamburg");
+
+INSERT INTO City (City_Name, Is_Capital, Has_Castle, Has_Market) VALUES ("Mancity", "True", "True", "False");
+INSERT INTO City (City_Name, Is_Capital, Has_Castle, Has_Market) VALUES ("OldYork", "True", "True", "False");
+INSERT INTO City (City_Name, Is_Capital, Has_Castle, Has_Market) VALUES ("NewYork", "True", "True", "False")
+INSERT INTO City (City_Name, Is_Capital, Has_Castle, Has_Market) VALUES ("Richland", "True", "True", "False")
